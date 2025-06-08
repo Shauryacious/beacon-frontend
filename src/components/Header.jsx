a00c526e8d11dbc9a042adb7f0749685aa4146ca
Shauryacious <shauryaagrawal2718@gmail.com>
2025-06-08 17:45:23 +0530

    fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,12 +13,11 @@ const Header = ({ theme, setTheme }) => {
       style={{ willChange: "transform" }}
     >
       <div className="flex items-center min-w-[160px]">
-        <svg className="w-8 h-8" viewBox="0 0 24 24" fill="#FF9900">
-          <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-1-13h2v6h-2zm0 8h2v2h-2z" />
-        </svg>
-        <span className="ml-2 font-bold text-xl text-white tracking-wide">
-          Trustify
-        </span>
+        <img
+          src="logo.png"
+          alt="BEACON Logo"
+          className="w-40 h-32 object-contain"
+        />
       </div>
 
       <div className="flex-1 flex justify-center mx-4">
